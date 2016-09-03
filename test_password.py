2d2ed7c31834907134a2da0b7c9eb42f801472b5
wdhg <wdhgrant@gmail.com>
2016-09-03 17:21:40 +0100

    Added test_password function

--- test_password.py
+++ test_password.py
@@ -6,6 +6,9 @@ def get_password(filename):
     password, salt = open(filename, "r").read().split()
     return (password, salt)
 
+def test_password(password, hashed_password, salt):
+    return hashed_password = sha512(password.encode() + salt.encode())
+
 def main():
     password = input("Enter password: ")
     hashed_password, salt = get_password(INPUT_FILE)
