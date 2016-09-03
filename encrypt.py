4c1b0aa82f32cc9fbb0a481ffe0bb051dde81455
wdhg <wdhgrant@gmail.com>
2016-09-03 17:12:51 +0100

    Added encrypt function

--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,13 @@ from hashlib import sha512
 from random import choice
 from string import ascii_letters
 
+SALT_LENGTH = 32
+
+def encrypt(password):
+    salt = "".join((choice(ascii_letters) for letter in range(SALT_LENGTH)))
+    hashed_password = sha512(password.encode() + salt.encode()).hexdigest()
+    return hashed_password
+
 def main():
     password = input("Enter password: ")
 
