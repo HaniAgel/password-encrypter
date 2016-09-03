59b7476d5c18671fc7f8df2abc3d6aa3b924d6c5
wdhg <wdhgrant@gmail.com>
2016-09-03 17:09:15 +0100

    Added main function

---
+++ encrypt.py
@@ -0,0 +1,9 @@
+from hashlib import sha512
+from random import choice
+from string import ascii_letters
+
+def main():
+    password = input("Enter password: ")
+
+if __name__ == "__main__":
+	main()
