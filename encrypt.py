215af48e7021d60a3dbee03c0135119bfe74cb7c
wdhg <wdhgrant@gmail.com>
2016-09-03 17:14:08 +0100

    Changed encrypt to return salt too

--- encrypt.py
+++ encrypt.py
@@ -7,7 +7,7 @@ SALT_LENGTH = 32
 def encrypt(password):
     salt = "".join((choice(ascii_letters) for letter in range(SALT_LENGTH)))
     hashed_password = sha512(password.encode() + salt.encode()).hexdigest()
-    return hashed_password
+    return (hashed_password, salt)
 
 def main():
     password = input("Enter password: ")
